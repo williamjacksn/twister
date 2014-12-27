d82982b8251774f454bb22151fdf10455d8e0ca3
William Jackson <william@subtlecoolness.com>
2014-12-27 08:54:27 -0600

    Add twister.py

---
+++ twister.py
@@ -0,0 +1,56 @@
+import argparse
+import random
+import subprocess
+import sys
+import time
+
+parser = argparse.ArgumentParser()
+
+air_help = 'include \'in the air\' as a valid action'
+parser.add_argument('-a', '--air', action='store_true', help=air_help)
+
+delay_help = ('number of seconds to wait before automatically showing the '
+              'next move (specifying -d with no value will use the default '
+              'value of 10 seconds)')
+parser.add_argument('-d', '--delay', nargs='?', const=10, type=int,
+                    help=delay_help)
+
+talk_help = ('speak the move aloud (only supported on platforms with the '
+             '`say` command)')
+parser.add_argument('-t', '--talk', action='store_true', help=talk_help)
+
+args = parser.parse_args()
+
+parts = ['Right Foot', 'Right Hand', 'Left Foot', 'Left Hand']
+actions = ['Red', 'Yellow', 'Green', 'Blue']
+
+if args.air:
+    print('Playing with \'in the air\' as a valid action')
+    actions.append('in the air')
+
+if args.delay:
+    m = 'Automatically showing next move after {} second'
+    if args.delay > 1:
+        m = '{}s'.format(m)
+    print(m.format(args.delay))
+else:
+    print('Press <Enter> to show the next move')
+
+if args.talk:
+    print('Talking is activated')
+
+print('**')
+
+while True:
+    try:
+        move = '{} {}'.format(random.choice(parts), random.choice(actions))
+        print(move)
+        if args.talk:
+            subprocess.call(['say', '-v', 'Samantha', '-r', '150', move])
+        if args.delay:
+            time.sleep(args.delay)
+        else:
+            input()
+    except KeyboardInterrupt:
+        print()
+        sys.exit()
